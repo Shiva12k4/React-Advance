4d75db5604e8d7f6a702c01d797796b1b36f95cc
Shiva2k14 <shivam.shsrma20@gmail.com>
2025-01-09 17:30:24 +0530

    react ad

---
+++ src/Context/UserContext.js
@@ -0,0 +1,10 @@
+//context - data ko globally store karta hai just like global variable ,jb kisi bhi component ko bina props pass kiye direct acces dene ki permission deta hai 
+
+
+import React from "react";
+
+const UserContext=React.createContext()
+
+export default UserContext;
+
+
